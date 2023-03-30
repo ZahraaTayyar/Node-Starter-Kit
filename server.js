c338af17a6204ea072dc3e2a316c854d2bea8799
ZahraaTayyar <zahraatayyar@gmail.com>
2023-03-30 17:30:25 +0100

    Lesson 2 completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("Yay Node!");
@@ -19,22 +20,74 @@ app.get("/chocolate", (request, response) => {
   response.send(amountQuery);
 });
 
-app.get("/json", function (req, res) {
-  let lat = req.query.lat;
-  let lng = req.query.lng;
-  res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
+app.get("/json", (request, response) => {
+  let lat = request.query.lat;
+  let lng = request.query.lng;
+  response.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
 });
 
-app.get("/multiply", (req, res) => {
+app.get("/multiply", (request, response) => {
   // the values from the query are always strings
-  const value1Query = Number(req.query.value1);
-  const value2Query = Number(req.query.value2);
+  const value1Query = Number(request.query.value1);
+  const value2Query = Number(request.query.value2);
   const multiplyResult = value1Query * value2Query;
   // this result is STILL A NUMBER and we cannot send that number to res.send() because...
   // if you give res.send() a NUMBER it will think its an HTTP CODE and then try to send it back as that - the same as doing res.status(30)
   // res.send(multiplyResult);  // <-- returning HTTP CODE 30
   // so we convert it back to a String
-  res.send(multiplyResult.toString());
+  response.send(multiplyResult.toString());
+});
+
+//Lesson 2 exercises:
+
+const albumsData = [
+  {
+    albumId: "10",
+    artistName: "Beyoncé",
+    collectionName: "Lemonade",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+    releaseDate: "2016-04-25T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+  {
+    albumId: "11",
+    artistName: "Beyoncé",
+    collectionName: "Dangerously In Love",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+    releaseDate: "2003-06-24T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+];
+
+app.get("/albums", (request, response) => {
+  response.status(200).send(albumsData);
+});
+
+app.get("/albums/:albumId", (request, response) => {
+  const idToFind = request.params.albumId;
+  const album = albumsData.find((album) => album.albumId === idToFind);
+  response.status(200).send({ album });
+});
+
+app.post("/albums", (request, response) => {
+  const newAlbum = request.body;
+  albumsData.push(newAlbum);
+  response.status(201).send({ newAlbum });
+});
+
+app.delete("/albums/:albumId", (request, response) => {
+  console.log("DELETE /albums route");
+
+  const idToDelete = request.params.albumId;
+  console.log(idToDelete, "<----- id to delete");
+  const albumIndex = albumsData.indexOf(idToDelete);
+  console.log(albumIndex, "<----- index of deleted item");
+  const removeId = albumsData.splice(albumIndex);
+  console.log(removeId, "<----- array of removed item?");
 });
 
 app.listen(3000, () => {
